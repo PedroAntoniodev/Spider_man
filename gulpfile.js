69e208fc209d886dddb4a947b21598c6f3438eef
Pedro <pedroantoniogoncalves02@gmail.com>
2024-12-17 13:42:57 -0300

    finalizando o projeto com novas imagens e arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts(){
@@ -22,7 +23,7 @@ function images() {
 
 
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js')
